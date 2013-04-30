45647ce43cc3e1552f3d261b2e0b8487488f5311
Mario Chamuty <mario@chamuty.com>
2013-04-30 15:44:03 +0200

    Zmeny v readme :)

---
+++ protected/runtime/gii-1.1.13/ModelCode.php
@@ -0,0 +1,9 @@
+<?php
+return array (
+  'template' => 'default',
+  'connectionId' => 'db',
+  'tablePrefix' => 'tst_',
+  'modelPath' => 'application.models',
+  'baseClass' => 'CActiveRecord',
+  'buildRelations' => '1',
+);
